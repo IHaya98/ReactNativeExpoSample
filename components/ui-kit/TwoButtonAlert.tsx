26047044d82019ca3bf94150e404908cfecf9669
IHaya98 <sakura98eight@gmail.com>
2021-05-04 19:59:49 +0900

    修正

--- components/ui-kit/TwoButtonAlert.tsx
+++ components/ui-kit/TwoButtonAlert.tsx
@@ -9,18 +9,48 @@ const TwoButtonAlert = (props: any) => {
     const toggleModal = props.toggleModal
     const id = props.id
     return (
-        <View style={{ justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Modal Content</Text>
-            <Button
-                title="Close modal"
-                onPress={toggleModal}
-            />
-            <Button
-                title="OK"
-                onPress={()=>dispatch(deletePost(id))}
-            />
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={styles.title}>削除しますか？</Text>
+            <View style={styles.button_grp}>
+                <View style={styles.button}>
+                    <Button
+                        title="削除"
+                        onPress={() => dispatch(deletePost(id))}
+                    />
+                </View>
+                <View style={styles.button}>
+                    <Button
+                        title="キャンセル"
+                        onPress={toggleModal}
+                    />
+                </View>
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        padding: 24,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 28,
+        alignSelf: 'center',
+        marginBottom: 24,
+    },
+    button_grp: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '60%',
+    },
+    button: {
+        marginLeft: 30,
+        marginRight: 30,
+    },
+});
 
 export default TwoButtonAlert;
